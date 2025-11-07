a77a08ff0adda37283c837e25123cb3f079dd7a5
Bích Ngọc <ngoc15082006@gmail.com>
2025-11-07 22:49:36 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,4 @@
+// Khi nhấn nút "Tìm hiểu thêm"
+document.getElementById("learnMoreBtn").addEventListener("click", function() {
+  alert("Bộ sách lớp 12 được biên soạn theo chương trình mới, giúp bạn ôn tập và thi tốt nghiệp THPT hiệu quả!");
+});
